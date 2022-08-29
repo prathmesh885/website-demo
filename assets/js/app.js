eca6bae1b61457c91aa29b1f03760136a2bc27e7
prathmesh885 <96106319+prathmesh885@users.noreply.github.com>
2022-08-29 19:55:11 +0530

    website

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+// 
+
+
+var a=10;
+var b="20"
+
+var z= a-b;
+
+// console.log(z)
+var x = 4;
+// x += 1;
+// console.log(x);
+
+// x -=1;
+// console.log(x);
+
+
+// x *=1;
+// console.log(x);
+
+// x /=5;
+// console.log(x);
+
+
+x %=11;
+console.log(x);
